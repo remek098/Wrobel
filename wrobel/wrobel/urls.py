3bb05d8b74cba8c3c09489a89b7693408e76c09e
intain <jan.bartnicki@gmail.com>
2023-01-04 12:10:44 +0100

    Dodanie strony wylogowywania

--- wrobel/wrobel/urls.py
+++ wrobel/wrobel/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('feed.urls')),
     path('login/', auth_views.LoginView.as_view(template_name = 'accounts/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name = 'accounts/logout.html'), name='logout'),
     path('register/', acc_views.register, name='register'),
 ]
