c098c11bbd3a0c018e2ace0bd4919ce5ebe19d48
intain <jan.bartnicki@gmail.com>
2023-01-04 13:20:33 +0100

    Dodanie tworzenia i edycji profilów

--- wrobel/accounts/views.py
+++ wrobel/accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
-from .forms import RegisterForm
+from .forms import RegisterForm, ProfileUpdateForm
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def register(request):
@@ -14,3 +15,19 @@ def register(request):
     else: 
         form = RegisterForm()
     return render(request, 'accounts/register.html', {'form':form})
+
+@login_required
+def profile(request):
+    return render(request, 'accounts/profile.html')
+
+def profile_update(request):
+    if request.method == 'POST':
+        profile_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        if profile_form.is_valid:
+            profile_form.save()
+            return redirect('profile')
+    else:
+        profile_form = ProfileUpdateForm(instance=request.user.profile)
+
+
+    return render(request, 'accounts/profile_update.html', {'profile_form':profile_form})
--- wrobel/wrobel/urls.py
+++ wrobel/wrobel/urls.py
@@ -18,10 +18,20 @@ from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from accounts import views as acc_views
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('feed.urls')),
     path('login/', auth_views.LoginView.as_view(template_name = 'accounts/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name = 'accounts/logout.html'), name='logout'),
     path('register/', acc_views.register, name='register'),
+    path('profile/', acc_views.profile, name='profile'),
+    path('profile_update/', acc_views.profile_update, name='profile_update')
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
